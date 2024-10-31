19f6149f155c0ef58f799ea2382922ebb1e6a1c5
jesus is king <fadi.b.alhabib@gmail.com>
2024-10-31 14:16:38 +0200

    merge

--- Template.Application/Products/Dtos/ImageResolver.cs
+++ Template.Application/Products/Dtos/ImageResolver.cs
@@ -1,31 +1,25 @@
 ﻿using AutoMapper;
-using AutoMapper.Execution;
-using MediatR;
-using System.IO;
 using Template.Application.Products.Commands.CreateProductCommand;
 using Template.Domain.Entities.Products;
 using Template.Domain.Repositories;
 
-namespace Template.Application.Products.Dtos
-{
-	public class ImageResolver(IFileService fileService) : IValueResolver<CreateProductCommand, Product, List<ProductImages>>
-	{
-		public List<ProductImages> Resolve(CreateProductCommand source, Product destination, List<ProductImages> destMember, ResolutionContext context)
-		{
-			List<ProductImages> productImages = [];
+namespace Template.Application.Products.Dtos;
 
-			List<string>? paths = fileService.SaveFilesAsync(source.Images, "Images/Products", [".jpg", ".png"]);
-			if(paths != null)
-			{
-				foreach (var path in paths)
-				{
-					productImages.Add(new ProductImages
-					{
-						ImagePath = path
-					});
-				}
-			}
-			return productImages;
-		}
-	}
+public class ImageResolver(IFileService fileService)
+    : IValueResolver<CreateProductCommand, Product, List<ProductImages>>
+{
+    public List<ProductImages> Resolve(CreateProductCommand source, Product destination, List<ProductImages> destMember,
+        ResolutionContext context)
+    {
+        List<ProductImages> productImages = [];
+
+        var paths = fileService.SaveFiles(source.Images, "Images/Products", [".jpg", ".png"]);
+        if (paths != null)
+            foreach (var path in paths)
+                productImages.Add(new ProductImages
+                {
+                    ImagePath = path
+                });
+        return productImages;
+    }
 }
--- Template.Domain/Repositories/ICriteriaRepository.cs
+++ Template.Domain/Repositories/ICriteriaRepository.cs
@@ -1,6 +1,14 @@
+using Template.Domain.Entities.Criteria;
+
 namespace Template.Domain.Repositories;
 
 public interface ICriteriaRepository
 {
+    public Task<IEnumerable<Criteria>> GetAllAsync();
+
+    public Task<Criteria> GetByIdAsync(int id);
+
+    public Task<int> CreateCriteriaAsync(Criteria criteria);
+
     
 }
