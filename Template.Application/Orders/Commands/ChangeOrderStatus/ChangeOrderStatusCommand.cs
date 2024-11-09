fe21e2b42d8b681eb3a9adfb247ccdc5b72076ea
Loukas998 <majdlouka4@gmail.com>
2024-11-09 03:01:31 +0200

    changing order status

---
+++ Template.Application/Orders/Commands/ChangeOrderStatus/ChangeOrderStatusCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Template.Application.Orders.Commands.ChangeOrderStatus
+{
+	public class ChangeOrderStatusCommand : IRequest
+	{
+		public int OrderId { get; set; }
+		public string NewStatus { get; set; } = default!;
+	}
+}
---
+++ Template.Application/Orders/Commands/ChangeOrderStatus/ChangeOrderStatusCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Template.Domain.Exceptions;
+using Template.Domain.Repositories;
+
+namespace Template.Application.Orders.Commands.ChangeOrderStatus
+{
+	public class ChangeOrderStatusCommandHandler(ILogger<ChangeOrderStatusCommandHandler> logger,
+		IOrderRepository orderRepository) : IRequestHandler<ChangeOrderStatusCommand>
+	{
+		public async Task Handle(ChangeOrderStatusCommand request, CancellationToken cancellationToken)
+		{
+			logger.LogInformation("changing order status with id: {OrderId} to status: {NewStatus}",
+				request.OrderId, request.NewStatus);
+
+			var order = await orderRepository.GetOrderById(request.OrderId);
+			if (order == null)
+			{
+				throw new NotFoundException(nameof(order), request.OrderId.ToString());
+			}
+			order.Status = request.NewStatus;
+			await orderRepository.SaveChangesAsync();
+		}
+	}
+}
