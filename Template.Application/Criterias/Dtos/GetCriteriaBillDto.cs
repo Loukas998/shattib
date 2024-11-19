7e582d6d5310a9ae65332e11b345faedb9855293
jesus is king <fadi.b.alhabib@gmail.com>
2024-11-19 09:27:23 +0200

    added default null to criteria bill

--- Template.Application/Criterias/Dtos/GetCriteriaBillDto.cs
+++ Template.Application/Criterias/Dtos/GetCriteriaBillDto.cs
@@ -5,5 +5,5 @@ public class GetCriteriaBillDto
     public int Id { get; set; }
     public string Image { get; set; } = default!;
     public string? Receipt { get; set; }
-    public bool? Accepted { get; set; }
+    public bool? Accepted  { get; set; }
 }
--- Template.Domain/Entities/Criterias/CriteriaBill.cs
+++ Template.Domain/Entities/Criterias/CriteriaBill.cs
@@ -6,7 +6,7 @@ public class CriteriaBill
     public string Image { get; set; } = default!;
     public string? Receipt { get; set; }
     public int CriteriaId { get; set; } = default!;
-    public bool? Accepted { get; set; }
+    public bool? Accepted { get; set; } = null;
 
     public Criteria Criteria { get; set; }
 }
